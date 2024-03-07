ec329aaea29605eb3fa9dc376b82c2524dae5807
sei-eschwartz <50171988+sei-eschwartz@users.noreply.github.com>
2024-03-07 20:46:26 +0000

    sample 250 files.

--- tests/src/oo/5e6a9a81a425d68b2efd0e59c98a2809.cpp
+++
@@ -1,8 +0,0 @@
-// type for special constructors
-struct special_constructor {};
-// definitions
-#include <iostream>
-void check_driver() {
- std::cerr << "SUCCESS" << std::endl;
-}
-int main() { check_driver (); }
