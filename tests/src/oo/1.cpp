4819a48100237527cab6e359c4d7d40d6a12271c
Edward J. Schwartz <edmcman@cmu.edu>
2024-07-02 13:25:10 -0400

    New counter-example

--- tests/src/oo/1.cpp
+++ tests/src/oo/1.cpp
@@ -9,7 +9,8 @@ struct C0  {
 C0 (special_constructor);
 };
 struct C1 : public virtual C0 {
-/* method id 401 */ C1 ();
+/* method id 401 */ virtual ~C1 ();
+/* method id 402 */ C1 ();
 C1 (special_constructor);
 C0 v0;
 };
@@ -19,7 +20,11 @@ C0 v0;
 }
 C0::C0 (special_constructor)  {}
 
-/* method id 401 */ C1::C1 () {
+/* method id 401 */ C1::~C1 () {
+return;
+
+}
+/* method id 402 */ C1::C1 () {
 
 }
 C1::C1 (special_constructor) : C0(special_constructor ()) {}
@@ -44,20 +49,21 @@ class C0    size(0)
  // verify base offset
 }
 /*
-class C1    size(8)
+class C1    size(12)
 0    +---
-0    | {vbptr}
-4    | var0: ClassType{0}
-5    | alignment: Self alignment padding for class 1 of 4 bytes
-8    +---
-8    +--- (virtual base class C0)
-8    +---
+0    | {vfptr}
+4    | {vbptr}
+8    | var0: ClassType{0}
+9    | alignment: Self alignment padding for class 1 of 4 bytes
+12   +---
+12   +--- (virtual base class C0)
+12   +---
 */
 {
  const C1* obj = new C1(special_constructor());
  // verify class size
  const size_t vc_size = sizeof(C1);
- const size_t layout_size = 8;
+ const size_t layout_size = 12;
  if (vc_size == layout_size) {
   std::cerr << "CLASSSIZE CORRECT: C1 " << layout_size << std::endl;
  } else {
@@ -67,7 +73,7 @@ class C1    size(8)
  {
  const C0* baseobj = (C0*) obj;
  const size_t computed_offset = (uintptr_t)baseobj - (uintptr_t)obj;
- const size_t model_offset = 8;
+ const size_t model_offset = 12;
  if (computed_offset == model_offset) {
   std::cerr << "BASEOFFSET CORRECT: C1 C0 " << computed_offset << std::endl;
  } else {
