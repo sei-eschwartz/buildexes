95a68d7a5191015fed974682253a298ff9bcc948
Edward J. Schwartz <edmcman@cmu.edu>
2024-07-02 16:32:21 -0400

    Add 2

---
+++ tests/src/oo/2.cpp
@@ -0,0 +1,183 @@
+// type for special constructors
+struct special_constructor {};
+struct abstract_0 { int x; };
+struct C0;
+
+struct C1;
+
+struct C2;
+
+struct C3;
+
+struct C0  {
+/* method id 400 */ virtual ~C0 ();
+/* method id 401 */ virtual abstract_0 func1 ();
+/* method id 402 */ C0 ();
+C0 (special_constructor);
+};
+struct C1 : public C0 {
+/* method id 403 */ C1 ();
+C1 (special_constructor);
+};
+struct C2 : public C1 {
+/* method id 404 */ C2 ();
+C2 (special_constructor);
+};
+struct C3 : public virtual C2 {
+/* method id 405 */ C3 ();
+C3 (special_constructor);
+};
+// definitions
+/* method id 400 */ C0::~C0 () {
+return;
+
+}
+/* method id 401 */ abstract_0 C0::func1 () {
+return abstract_0();
+
+}
+/* method id 402 */ C0::C0 () {
+
+}
+C0::C0 (special_constructor)  {}
+
+/* method id 403 */ C1::C1 () {
+return;
+
+}
+C1::C1 (special_constructor) : C0(special_constructor ()) {}
+
+/* method id 404 */ C2::C2 () {
+return;
+
+}
+C2::C2 (special_constructor) : C1(special_constructor ()) {}
+
+/* method id 405 */ C3::C3 () {
+
+}
+C3::C3 (special_constructor) : C2(special_constructor ()) {}
+
+#include <iostream>
+void check_driver() {
+/*
+class C0    size(4)
+0    +---
+0    | {vfptr}
+4    +---
+*/
+{
+ const C0* obj = new C0(special_constructor());
+ // verify class size
+ const size_t vc_size = sizeof(C0);
+ const size_t layout_size = 4;
+ if (vc_size == layout_size) {
+  std::cerr << "CLASSSIZE CORRECT: C0 " << layout_size << std::endl;
+ } else {
+  std::cerr << "CLASSSIZE INCORRECT: C0 " << vc_size << " " << layout_size << std::endl;
+ }
+ // verify base offset
+}
+/*
+class C1    size(4)
+0    +---
+0    | +--- (base class C0)
+0    | | {vfptr}
+4    | +---
+4    +---
+*/
+{
+ const C1* obj = new C1(special_constructor());
+ // verify class size
+ const size_t vc_size = sizeof(C1);
+ const size_t layout_size = 4;
+ if (vc_size == layout_size) {
+  std::cerr << "CLASSSIZE CORRECT: C1 " << layout_size << std::endl;
+ } else {
+  std::cerr << "CLASSSIZE INCORRECT: C1 " << vc_size << " " << layout_size << std::endl;
+ }
+ // verify base offset
+ {
+ const C0* baseobj = (C0*) obj;
+ const size_t computed_offset = (uintptr_t)baseobj - (uintptr_t)obj;
+ const size_t model_offset = 0;
+ if (computed_offset == model_offset) {
+  std::cerr << "BASEOFFSET CORRECT: C1 C0 " << computed_offset << std::endl;
+ } else {
+  const size_t vc_base_size = sizeof(C0);
+  if (vc_base_size == 0 || vc_base_size == 1) std::cerr << "BASEOFFSET WEIRD: C1 C0 ";
+  else std::cerr << "BASEOFFSET INCORRECT: C1 C0 ";
+  std::cerr << computed_offset << " " << model_offset << std::endl;
+ }}}
+/*
+class C2    size(4)
+0    +---
+0    | +--- (base class C1)
+0    | | +--- (base class C0)
+0    | | | {vfptr}
+4    | | +---
+4    | +---
+4    +---
+*/
+{
+ const C2* obj = new C2(special_constructor());
+ // verify class size
+ const size_t vc_size = sizeof(C2);
+ const size_t layout_size = 4;
+ if (vc_size == layout_size) {
+  std::cerr << "CLASSSIZE CORRECT: C2 " << layout_size << std::endl;
+ } else {
+  std::cerr << "CLASSSIZE INCORRECT: C2 " << vc_size << " " << layout_size << std::endl;
+ }
+ // verify base offset
+ {
+ const C1* baseobj = (C1*) obj;
+ const size_t computed_offset = (uintptr_t)baseobj - (uintptr_t)obj;
+ const size_t model_offset = 0;
+ if (computed_offset == model_offset) {
+  std::cerr << "BASEOFFSET CORRECT: C2 C1 " << computed_offset << std::endl;
+ } else {
+  const size_t vc_base_size = sizeof(C1);
+  if (vc_base_size == 0 || vc_base_size == 1) std::cerr << "BASEOFFSET WEIRD: C2 C1 ";
+  else std::cerr << "BASEOFFSET INCORRECT: C2 C1 ";
+  std::cerr << computed_offset << " " << model_offset << std::endl;
+ }}}
+/*
+class C3    size(8)
+0    +---
+0    | {vbptr}
+4    +---
+4    +--- (virtual base class C2)
+4    | +--- (base class C1)
+4    | | +--- (base class C0)
+4    | | | {vfptr}
+8    | | +---
+8    | +---
+8    +---
+*/
+{
+ const C3* obj = new C3(special_constructor());
+ // verify class size
+ const size_t vc_size = sizeof(C3);
+ const size_t layout_size = 8;
+ if (vc_size == layout_size) {
+  std::cerr << "CLASSSIZE CORRECT: C3 " << layout_size << std::endl;
+ } else {
+  std::cerr << "CLASSSIZE INCORRECT: C3 " << vc_size << " " << layout_size << std::endl;
+ }
+ // verify base offset
+ {
+ const C2* baseobj = (C2*) obj;
+ const size_t computed_offset = (uintptr_t)baseobj - (uintptr_t)obj;
+ const size_t model_offset = 4;
+ if (computed_offset == model_offset) {
+  std::cerr << "BASEOFFSET CORRECT: C3 C2 " << computed_offset << std::endl;
+ } else {
+  const size_t vc_base_size = sizeof(C2);
+  if (vc_base_size == 0 || vc_base_size == 1) std::cerr << "BASEOFFSET WEIRD: C3 C2 ";
+  else std::cerr << "BASEOFFSET INCORRECT: C3 C2 ";
+  std::cerr << computed_offset << " " << model_offset << std::endl;
+ }}}
+ std::cerr << "SUCCESS" << std::endl;
+}
+int main() { check_driver (); }
